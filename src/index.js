c1b4a389193853504270eece4b5c102bced2ee96
Stephen Pflug <stephen@pflug.us>
2023-02-03 10:10:25 -0600

    Extracting Components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 // import react and reactDOM libraries
 import React from "react";
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 // get a reference to div with id root
 const el = document.getElementById("root");
 
 // tell react to take control of that element
 const root = ReactDOM.createRoot(el);
-
-// create component
-function App() {
-
-    return (
-        <textarea autoFocus={true} />
-    );
-}
 
 // show component on screen
 root.render(<App />);
